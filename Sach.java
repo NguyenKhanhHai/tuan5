bc0afa0ce0b8fa647974f0eb368a6e29783b266d
NguyenKhanhHai <haikpdz@gmail.com>
2025-10-04 21:10:56 +0700

    Add files via upload

---
+++ Sach.java
@@ -0,0 +1,81 @@
+public class Sach {
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+
+    public Sach() {
+    }
+
+     public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTieuDe() {
+        return tieuDe;
+    }
+
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Thông tin sách:");
+        System.out.println("Mã sách: " + maSach);
+        System.out.println("Tiêu đề: " + tieuDe);
+        System.out.println("Tác giả: " + tacGia);
+        System.out.println("Năm xuất bản: " + namXuatBan);
+        System.out.println("Số lượng: " + soLuong);
+    }
+
+
+    public static void main(String[] arg){
+        Sach sach1 = new Sach("S001", "Lập trình Java", "Nguyễn Khánh Hải", 2025, 5);
+        Sach sach2 = new Sach();
+        sach2.setMaSach("S002");
+        sach2.setTieuDe("Cấu trúc dữ liệu");
+        sach2.setTacGia("Nguyễn Lý Luật");
+        sach2.setNamXuatBan(2020);
+        sach2.setSoLuong(3);
+
+        sach1.hienThiThongTin();
+        sach2.hienThiThongTin();
+    }
+}
